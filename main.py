86c299abc09317868fbb7ec23d9c5b01e6476d4f
sushiyiv <309829899@qq.com>
2026-07-15 23:33:45 +0800

    chore: 更新版本号到 v1.1.1

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main():
     app.setFont(font)
 
     app.setApplicationName("穷观阵")
-    app.setApplicationVersion("1.1.0")
+    app.setApplicationVersion("1.1.1")
     app.setOrganizationName("QianGuanZhen")
 
     window = MainWindow()
